0c0cad8d3c63edd8d11c22d538a650342ce2d713
Teodor Muzychuk <teodor.muzychuk@ucu.edu.ua>
2023-03-31 17:55:39 +0300

    poop

---
+++ main.cpp
@@ -0,0 +1,9 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
